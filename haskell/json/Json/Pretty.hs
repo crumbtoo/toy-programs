20b25ee260d613c1852ab741fa68302b6f5fe047
Madeline Slaga <crumble@fruitbook.local>
2023-02-15 13:19:26 -0700

    json

--- haskell/json/Json/Pretty.hs
+++ haskell/json/Json/Pretty.hs
@@ -1,5 +1,7 @@
 module Json.Pretty where
 
+#define INDENT_WIDTH 4
+
 unroll :: String -> String
 unroll "" = ""
 unroll (s:st) =
@@ -31,28 +33,31 @@ unroll (s:st) =
 
 showDepth :: Int -> String
 showDepth 0 = ""
-showDepth d = "  " ++ showDepth (d - 1)
+showDepth d = tab ++ showDepth (d - 1)
+    where tab = replicate INDENT_WIDTH ' '
 
 indent' :: [String] -> Int -> [String]
 indent' [] _ = []
-indent' (x@(s:st):xs) depth =
-    case s of
-        '{' ->
+indent' (x@(s:st):xs) depth
+    | s == '{' || s == '[' =
             (showDepth depth ++ x) : indent' xs (depth + 1)
-
-        '}' ->
+    | s == '}' || s == ']' =
             (showDepth (depth - 1) ++ x) : indent' xs (depth - 1)
-
-        '[' ->
-            (showDepth depth ++ x) : indent' xs (depth + 1)
-        ']' ->
-            (showDepth (depth - 1) ++ x) : indent' xs (depth - 1)
-
-        _ ->
-            (showDepth depth ++ x) : indent' xs depth
+    | otherwise =
+        (showDepth depth ++ x) : indent' xs depth
 
 indent :: String -> String
-indent s = unlines $ indent' (lines s) 0
+indent s = take (length iota - 1) iota
+    where iota = unlines $ indent' (lines s) 0
+-- `take (length iota - 1)` removes the last element from the list,
+-- which is an unwanted newline that `unlines` adds
+
+space :: String -> String
+space "" = ""
+space [s] = [s]
+space (s:w@(c:st)) = if s == ':' && c /= '\n'
+               then ": " ++ space w
+               else s : space w
 
 beautify :: String -> String
-beautify s = (indent.unroll) s
+beautify s = (space.indent.unroll) s
