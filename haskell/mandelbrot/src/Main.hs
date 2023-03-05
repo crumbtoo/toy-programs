9753433885cdfe20b6ff3c337bd8fa39516a0d2d
Madeline Slaga <crumble@fruitbook.local>
2023-03-05 00:00:56 -0700

    circle

--- haskell/mandelbrot/src/Main.hs
+++ haskell/mandelbrot/src/Main.hs
@@ -7,27 +7,33 @@ import Text.Printf
 type RGB = (Int, Int, Int)
 
 width, height :: Num a => a
-width = 1920
-height = 1080
+width = 128
+height = 128
 
 main :: IO ()
 main = do
-    let points = [ (x,y) | x <- [1..width], y <- [1..height] ]
+    let points = [ (x,y) | y <- [1..height], x <- [1..width] ]
     putStr $ mkHeader width height
     mapM_ (\ (x,y) ->
             let (r,g,b) = pointToRGB x y in
-            putStrLn $ printf "%d %d %d" r g b
+            putStrLn $ printf "%d %d %d # x: %d   y: %d" r g b x y
         ) points
 
 pointToRGB :: Int -> Int -> RGB
 pointToRGB x y =
-    ( round $ 255 * (rx / width)
-    , round $ 255 * (ry / height)
-    , round $ 255 * (rx + ry / width + height)
+    ( value
+    , value
+    , value
     )
     where
+        cx, cy :: Float
+        cx = width / 2.0
+        cy = height / 2.0
+        d = sqrt $ (cx - rx)^2 + (cy - ry)^2
+        r = 20
         rx = fromIntegral x
         ry = fromIntegral y
+        value = if d < r then 255 else 0
 
 mkHeader :: Int -> Int -> String
 mkHeader = printf "P3 %d %d 255\n"
